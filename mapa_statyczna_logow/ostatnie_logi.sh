acf01bed8443e7e977f9adf966c1153f519ad321
Mathieu <github@kumy.net>
2019-01-26 20:41:03 +0100

    Use Google map api key to generate last move picture

--- mapa_statyczna_logow/ostatnie_logi.sh
+++ mapa_statyczna_logow/ostatnie_logi.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 /usr/local/bin/php ./ostatnie_logi.php
+. ../konfig-tools.sh
 
 markers=`cat ./lastlogs.txt`
 
-url="http://maps.googleapis.com/maps/api/staticmap?size=500x500&markers=size:small|$markers&sensor=false"
+url="http://maps.googleapis.com/maps/api/staticmap?size=500x500&markers=size:small|$markers&sensor=false&key=$GOOGLE_MAP_KEY"
 
 echo $url;
 
-. ../konfig-tools.sh
 wget "$url" -O ${GEOKRETY_WWW}mapki/google_static_logs.png
