28842ea29b3f23b1b08e6cc328ae63967b5e6b5e
Boly38 <boly38@gmail.com>
2019-04-17 21:04:44 +0200

    update geonames endpoint and arg
    - fixes geokrety/geokrety-website#284

--- geonames/geonames-home-country.php
+++ geonames/geonames-home-country.php
@@ -4,48 +4,45 @@
 include_once("../konfig-tools.php");
 include_once("$geokrety_www/templates/konfig.php");
 require_once "$geokrety_www/__sentry.php";
-
-$link = DBPConnect();
-
-
-/* ----------------------------------------------- kraj ------------------------------------------------------- */
-
 include("$geokrety_www/get_country_from_coords.php");
+include("geonames_api.php");
 
 
+// --- kraj - country - get user without country but with home coordinates
+$userWithoutCountrySql = "SELECT `userid`, `lat`, `lon` FROM `gk-users` WHERE `lat` != '' and `lon` != '' and (country ='' or country is null)";
+$nbUserUpdated=0;
 
-//ok:
-$result = mysqli_query($link, "SELECT `userid`, `lat`, `lon` FROM `gk-users` WHERE `lat` != '' and `lon` != '' and country =''");
+$link = GKDB::getLink();
+$result = mysqli_query($link,$userWithoutCountrySql);
+if ($result === FALSE) {
+  echo 'invalid query:'.mysqli_error($link).'\n';
+  die;
+}
 
 while ($row = mysqli_fetch_array($result)) {
     list($userid, $lat, $lon) = $row;
-
-
     $country='';
-
     echo "$userid, $lat, $lon::  ";
-
+    // first try from geo.kumy.org
     $country=get_country_from_coords($lat, $lon);
     echo "[ $country] ";
 
     if ($country == '' or $country=='xyz' or $country==null) {
-        $handle = fopen("http://ws.geonames.org/countryCode?lat=$lat&lng=$lon&radius=30", "rb");
-        $country = strtolower(trim(fread($handle, 3)));
-        echo "geonames...";
+        // in case of failure, ask to geonames
+        $country = geonames_country_from_coords($geonamesEndpoint, $geonamesUsername, $lat, $lon);
         if (($country == '<!d') or ($country == '')) {
             $country = 'xyz';
             echo "bach!\n";
         }
-        fclose($handle);
         sleep(2);
     }
-
-
     echo "$country\n";
 
+    // update user with country
     $sql = "UPDATE `gk-users` SET  `country`='$country' WHERE `userid` = '$userid' LIMIT 1";
     $result2 = mysqli_query($link, $sql);
+    $nbUserUpdated++;
 }
-
-
 mysqli_close($link);
+
+echo "nbUserUpdated: $nbUserUpdated\n";
