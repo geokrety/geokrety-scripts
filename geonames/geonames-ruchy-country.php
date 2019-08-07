6bc47741769d139df4769ce1092ee84bba87b2b9
Mathieu <github@kumy.net>
2019-08-07 18:59:21 +0200

    Fix geokrety/geokrety-website/issues/452

--- geonames/geonames-ruchy-country.php
+++ geonames/geonames-ruchy-country.php
@@ -6,48 +6,47 @@ include_once("$geokrety_www/templates/konfig.php");
 require_once "$geokrety_www/__sentry.php";
 include("$geokrety_www/get_country_from_coords.php");
 
+const START_RUCHY_ID = 1809223; // TODO move this in general konfig
+
 // --- kraj - country - get ruchy without country but with coordinates
 
-$link = GKDB::getLink();
 $nbUpdated=0;
 // ruchy.logtype   	0=drop, 1=grab, 2=comment, 3=met, 4=arch, 5=dip
 
 // get ruchy that are not comment or grab and without country but with lat/lon
 $selectRuchyWithoutCountry = <<<EOSQL
-SELECT ruch_id, lat, lon FROM `gk-ruchy`
-WHERE `logtype`!='2' AND `logtype`!='1'
-  AND (`lat`!='0' AND `lon`!='0')
-  AND (`country` IS NULL or `country`='' or `country`='xyz' or `country`='err')
+SELECT  ruch_id, lat, lon
+FROM    `gk-ruchy`
+WHERE   logtype != '2'
+AND     logtype != '1'
+AND     lat != 0
+AND     lon != 0
+AND     (country = '' or country = 'xyz' or country = 'err')
+AND     ruch_id > ?
 EOSQL;
 
-$startRuchyId = 730000;
-if (isset($startRuchyId)) {
-  $selectRuchyWithoutCountry .= " AND `ruch_id`>$startRuchyId";
-}
+$sql = 'UPDATE `gk-ruchy` SET  `country` = ? WHERE ruch_id = ? LIMIT 1';
+$stmt = \GKDB::prepareBindExecute('selectRuchyWithoutCountry', $selectRuchyWithoutCountry, 'i', array(START_RUCHY_ID));
 
-$result = mysqli_query($link, $selectRuchyWithoutCountry);
-if ($result === FALSE) {
-  echo 'invalid query:'.mysqli_error($link).'\n';
-  die;
-}
-
-while ($row = mysqli_fetch_array($result)) {
-    list($ruch_id, $lat, $lon) = $row;
+$stmt->store_result();
+$stmt->bind_result(
+    $ruch_id,
+    $lat,
+    $lon
+);
+while ($stmt->fetch()) {
     $country='';
     echo "$ruch_id, $lat, $lon::  ";
 
-    // get from geo.kumy.org
+    // first get from geo.geokrety.org
     $country=get_country_from_coords($lat, $lon);
 
     echo "[ $country] ";
 
-    echo " google...";
     if ($country == '' or $country=='xyz' or $country==null or $country=='?') {
-        //$url="http://maps.google.com/maps/geo?q=$lat,$lon&output=json&sensor=false";
-        $url="https://maps.googleapis.com/maps/api/geocode/json?latlng=$lat,$lon&sensor=false";
-        $data = @file_get_contents($url);
-        $jsondata = json_decode($data, true);
-        //if(is_array($jsondata )&& $jsondata ['Status']['code']==200)  $country = $jsondata ['Placemark'][0]['AddressDetails']['Country']['CountryNameCode'];
+        echo " try from google…";
+        $url="https://maps.googleapis.com/maps/api/geocode/json?latlng=$lat,$lon&sensor=false&key=$GOOGLE_MAP_KEY";
+        $jsondata = json_decode(@file_get_contents($url), true);
         echo "status: "  . $jsondata['status'] . "\t";
 
         if (is_array($jsondata) and $jsondata['status']=="OK") {
@@ -55,7 +54,6 @@ while ($row = mysqli_fetch_array($result)) {
             foreach ($jsondata['results']['0']['address_components'] as $element) {
                 $data[ implode(' ', $element['types']) ] = $element['short_name'];
             }
-            //print_r($data);
 
             $country = $data['country political'];
         }
@@ -64,17 +62,18 @@ while ($row = mysqli_fetch_array($result)) {
     }
 
     $country = strtolower($country);
-    if ($country == '') {
-        $country ='xyz';
+    if ($country == 'xyz' or $country == '') {
+        echo " no luck, giving up!";
+        $country = null;
     }
 
-    echo "$country\n";
+    echo "[$country]\n";
 
+    $sql = 'UPDATE `gk-ruchy` SET  country = ? WHERE ruch_id = ? LIMIT 1';
+    $stmtUpdate = \GKDB::prepareBindExecute('updateRuchyCountry', $sql, 'si', array($country, $ruch_id));
+    $stmtUpdate->close();
     $nbUpdated++;
-    $sql = "UPDATE `gk-ruchy` SET  `country`='$country' WHERE `ruch_id` = '$ruch_id' LIMIT 1";
-    $result2 = mysqli_query($link, $sql);
 }
-mysqli_close($link);
+$stmt->close();
 
 echo "nbUpdated: $nbUpdated\n";
-?>
